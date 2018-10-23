3cf40513019166dfcc9597bf379540c885a6bc9c
Michael Nathan <realmichaelnathan@protonmail.com>
2018-10-22 23:40:24 -0500

    carousel indicator correction

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                         <span class="sr-only">Previous</span>
                     </a>
                     
-                    <ol class="carousel-indicators p-1 m-1">
+                    <ol class="carousel-indicators p-1 m-1 force-btm">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
