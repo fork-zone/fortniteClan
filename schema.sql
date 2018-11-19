8c053922ab3c3feb56491c27e6cf091844eb8b22
Mike <xxzukovxx@gmail.com>
2018-11-18 23:18:16 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
+/* Create users table */
 CREATE TABLE users (
   id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
 );
-
+/* Create clans table */
 CREATE TABLE clans (
   id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
   userid INT NOT NULL,
@@ -15,6 +16,7 @@ CREATE TABLE clans (
   website VARCHAR(255),
 );
 
+/* Create link between the users and clans table */
 ALTER TABLE clans 
 ADD CONSTRAINT user-clan 
 FOREIGN KEY (userid) 
